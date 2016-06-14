66099d08221a19de827532196b3390a6de99f9d0
beechan <hisobe@netprotections.co.jp>
2016-06-14 02:41:07 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    #Messageをすべて取得する
+    @messages = Message.all
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージをほぞんしました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ##ここまで
 end
